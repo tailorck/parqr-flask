c2daeb0606b7f611e7d290ed35a91f468ac80966
Girish Murali <gmurali6@gatech.edu>
2018-04-22 12:10:03 -0400

    Craeted 3 api endpoints for stats module

--- app/api.py
+++ app/api.py
@@ -14,6 +14,9 @@ from app import app
 from app.parser import Parser
 from app.modeltrain import ModelTrain
 from app.models import Course, Event, EventData
+from app.statistics import get_unique_users, number_posts_prevented, \
+    total_posts_in_course, get_top_attention_warranted_posts, is_course_id_valid
+
 from tasksrq import parse_posts, train_models
 from exception import InvalidUsage, to_dict
 from parqr import Parqr
@@ -167,3 +170,43 @@ def deregister_class():
         return jsonify({'course_id': cid}), 202
     else:
         raise InvalidUsage('Course ID does not exists', 500)
+
+
+@app.route(api_endpoint + 'class/cid/usage', methods=['GET'])
+def get_parqr_stats():
+    course_id = request.args.get('course_id')
+    start_time = request.args.get('start_time')
+    num_active_uid = get_unique_users(course_id, start_time)
+    num_post_prevented = number_posts_prevented(course_id, start_time)
+    num_total_posts = total_posts_in_course(course_id, start_time)
+    if num_active_uid == -1 or num_post_prevented == -1 \
+            or num_total_posts == -1:
+        raise InvalidUsage('Invalid Course ID / Start Time', 500)
+    else:
+        num_all_post = float(num_total_posts + num_post_prevented)
+        percent_traffic_reduced = num_post_prevented / num_all_post
+        return jsonify({'usingParqr': num_active_uid,
+                        'assistedCount': num_post_prevented,
+                        'percentTrafficReduced': percent_traffic_reduced}), 202
+
+
+@app.route(api_endpoint + '/api/class/cid/attentionposts', methods=['GET'])
+def get_top_posts():
+    course_id = request.args.get('course_id')
+    start_time = request.args.get('start_time')
+    num_posts= request.args.get('num_posts')
+    posts = get_top_attention_warranted_posts(course_id, start_time, num_posts)
+    if posts == -1:
+        raise InvalidUsage('Invalid Course ID / Start Time', 500)
+    else:
+        return jsonify({'posts': posts}), 202
+
+
+@app.route(api_endpoint + '/api/class/isvalid', methods=['GET'])
+def get_course_isvalid():
+    course_id = request.args.get('course_id')
+    is_valid = is_course_id_valid(course_id)
+    return jsonify({'valid': is_valid}), 202
+
+
+
