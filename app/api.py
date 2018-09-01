01a388236ab0c7fe7b5a1931f843f11a446f5a44
Roy <roy.finkelberg@gmail.com>
2018-09-01 15:40:04 -0400

    Filter notes from posts

--- app/api.py
+++ app/api.py
@@ -153,23 +153,23 @@ def deregister_class():
 def get_parqr_stats(course_id):
     try:
         start_time = int(request.args.get('start_time'))
-    except ValueError:
+    except ValueError, TypeError:
         raise InvalidUsage('Invalid start time specified', 400)
     num_active_uid = get_unique_users(course_id, start_time)
-    num_post_prevented = number_posts_prevented(course_id, start_time)
+    # num_post_prevented = number_posts_prevented(course_id, start_time)
     num_total_posts = total_posts_in_course(course_id)
-    num_all_post = float(num_total_posts + num_post_prevented)
-    percent_traffic_reduced = (num_post_prevented / num_all_post) * 100
-    return jsonify({'usingParqr': num_active_uid,
-                    'assistedCount': num_post_prevented,
-                    'percentTrafficReduced': percent_traffic_reduced}), 202
+    # num_all_post = float(num_total_posts + num_post_prevented)
+    # percent_traffic_reduced = (num_post_prevented / num_all_post) * 100
+    return jsonify({'usingParqr': num_active_uid,}), 202
+                    # 'assistedCount': num_post_prevented,
+                    # 'percentTrafficReduced': percent_traffic_reduced}), 202
 
 
 @app.route(api_endpoint + 'class/<course_id>/attentionposts', methods=['GET'])
 def get_top_posts(course_id):
     try:
         num_posts = int(request.args.get('num_posts'))
-    except ValueError:
+    except ValueError, TypeError:
         raise InvalidUsage('Invalid number of posts specified', 400)
     posts = get_top_attention_warranted_posts(course_id, num_posts)
     return jsonify({'posts': posts}), 202
