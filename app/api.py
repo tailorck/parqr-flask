cd8fca861e8ba3e99d82f58acc25eb52a83e6632
Bryan Baek <seungkwan.baek@gmail.com>
2019-09-24 17:51:43 -0400

    finished adding resources. next: testing

--- app/api.py
+++ app/api.py
@@ -12,11 +12,15 @@ from rq_scheduler import Scheduler
 from flask_cors import CORS
 from flask_restful import Api
 from app.resources.Course import Course
-from app.resources.Course import Course
-from app.resources.Course import Course
-from app.resources.Course import Course
-from app.resources.Course import Course
-from app.resources.Course import Course
+from app.resources.Course_Enrolled import Course_Enrolled
+from app.resources.Course_Supported import Course_Supported
+from app.resources.Course_Validator import Course_Validator
+from app.resources.Event import Event
+from app.resources.Instructor_Query import Instructor_Query
+from app.resources.Parser import Parser
+from app.resources.Query import Query
+from app.resources.Top_Post import Top_Post
+from app.resources.User import User
 
 
 from app import app
@@ -27,7 +31,7 @@ from app.exception import InvalidUsage, to_dict
 from app.parser import Parser
 from app.parqr import Parqr
 
-api_endpoint = '/api/'
+api_endpoint = '/api/2.0'
 
 parqr = Parqr()
 parser = Parser()
@@ -64,11 +68,18 @@ def identity(payload):
 
 jwt = JWT(app, verify, identity)
 
-api.add_resource(Item, '/item/<string:name>')
-api.add_resource(ItemList, '/items')
-api.add_resource(UserRegister, '/register')
-api.add_resource(Store, '/store/<string:name>')
-api.add_resource(StoreList, '/stores')
+api.add_resource(Course, api_endpoint + '/course/<string:course_id>')
+api.add_resource(Course_Enrolled, api_endpoint + '/courses_enrolled')
+api.add_resource(Course_Supported, api_endpoint + '/courses_supported')
+api.add_resource(Course_Validator, api_endpoint + '/isvalid')
+api.add_resource(Event, api_endpoint + '/event')
+api.add_resource(Instructor_Query, api_endpoint + '/instructor_recommendations')
+api.add_resource(Parser, api_endpoint + '/parse')
+api.add_resource(Query, api_endpoint + '/similar_posts')
+api.add_resource(Top_Post, api_endpoint + '/top_post/<string:course_id>/<string:user>')
+api.add_resource(User, api_endpoint + '/users')
+
+
 
 ###########################################################################
 
