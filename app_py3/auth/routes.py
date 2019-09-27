56a0ff5b0f9ac952119a027a0abcb4a2d5a17c12
Bryan Baek <seungkwan.baek@gmail.com>
2019-09-27 18:22:55 -0400

    deleted draft version of py3 restful

--- app_py3/auth/routes.py
+++
@@ -1,8 +0,0 @@
-
-from app.auth import bp
-from app.models import User
-
-# defining routes in a blueprint
-@bp.route('/login', methods=['GET', 'POST'])
-def login():
-    return None
--- app_py3/resources/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-from utils import create_app
-
-
-app = create_app(os.environ['FLASK_CONF'])
