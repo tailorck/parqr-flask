2862502a29b3f8ec785793cafbd44494f5ef0c93
tailorck <cktailor34@gmail.com>
2020-03-31 23:23:16 -0400

    rever model_cache changes

--- app/model_cache.py
+++ app/model_cache.py
@@ -15,6 +15,8 @@ class ModelCache(object):
 
     def store_model(self, cid, name, model):
         key = self.model_key_format.format(cid, name)
+        with open(self.tmp + key, 'wb') as f:
+            pickle.dump(model, f)
         self.s3.put_object(
             Bucket="parqr-models",
             Key=key,
@@ -23,6 +25,8 @@ class ModelCache(object):
 
     def store_matrix(self, cid, name, matrix):
         key = self.matrix_key_format.format(cid, name)
+        with open(self.tmp + key, 'wb') as f:
+            pickle.dump(matrix, f)
         self.s3.put_object(
             Bucket="parqr-models",
             Key=key,
@@ -31,6 +35,8 @@ class ModelCache(object):
 
     def store_pid_list(self, cid, name, pid_list):
         key = self.pid_list_key_format.format(cid, name)
+        with open(self.tmp + key, 'wb') as f:
+            pickle.dump(pid_list, f)
         self.s3.put_object(
             Bucket="parqr-models",
             Key=key,
@@ -47,35 +53,59 @@ class ModelCache(object):
     def get_model(self, cid, name):
         key = self.model_key_format.format(cid, name)
 
-        try:
-            self.s3.download_file("parqr-models", key, self.tmp + key)
-        except botocore.exceptions.ClientError as e:
-            print("Could not find MODEL for cid '{}' with "
-                  "name '{}'".format(cid, name))
-            model = None
+        if os.path.exists(self.tmp + key):
+            print("Loading model found in /tmp")
+            with open(self.tmp + key, "rb") as input_file:
+                model = pickle.load(input_file)
+        else:
+            try:
+                self.s3.download_file("parqr-models", key, self.tmp + key)
+            except botocore.exceptions.ClientError as e:
+                print("Could not find MODEL for cid '{}' with "
+                      "name '{}'".format(cid, name))
+                model = None
+            else:
+                print("Downloaded model from s3")
+                with open(self.tmp + key, "rb") as input_file:
+                    model = pickle.load(input_file)
 
         return model
 
     def get_matrix(self, cid, name):
         key = self.matrix_key_format.format(cid, name)
 
-        try:
-            self.s3.download_file("parqr-models", key, self.tmp + key)
-        except botocore.exceptions.ClientError as e:
-            print("Could not find MODEL for cid '{}' with "
-                  "name '{}'".format(cid, name))
-            matrix = None
-
+        if os.path.exists(self.tmp + key):
+            print("Loading matrix found in /tmp")
+            with open(self.tmp + key, "rb") as input_file:
+                matrix = pickle.load(input_file)
+        else:
+            try:
+                self.s3.download_file("parqr-models", key, self.tmp + key)
+            except botocore.exceptions.ClientError as e:
+                print("Could not find MODEL for cid '{}' with "
+                      "name '{}'".format(cid, name))
+                matrix = None
+            else:
+                print("Downloaded matrix from s3")
+                with open(self.tmp + key, "rb") as input_file:
+                    matrix = pickle.load(input_file)
         return matrix
 
     def get_pid_list(self, cid, name):
         key = self.pid_list_key_format.format(cid, name)
-
-        try:
-            self.s3.download_file("parqr-models", key, self.tmp + key)
-        except botocore.exceptions.ClientError as e:
-            print("Could not find MODEL for cid '{}' with "
-                  "name '{}'".format(cid, name))
-            pid_list = None
-
+        if os.path.exists(self.tmp + key):
+            print("Loading pid_list found in /tmp")
+            with open(self.tmp + key, "rb") as input_file:
+                pid_list = pickle.load(input_file)
+        else:
+            try:
+                self.s3.download_file("parqr-models", key, self.tmp + key)
+            except botocore.exceptions.ClientError as e:
+                print("Could not find MODEL for cid '{}' with "
+                      "name '{}'".format(cid, name))
+                pid_list = None
+            else:
+                print("Downloaded pid_list from s3")
+                with open(self.tmp + key, "rb") as input_file:
+                    pid_list = pickle.load(input_file)
         return pid_list
